21d069b53e9f13137eec4ce6c2061e9ce565afc9
gate3 <youngenterpreneur11@gmail.com>
2018-06-04 18:36:51 +0100

    Updating readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,7 @@ export class AppComponent {
 
   constructor (private geoService:GeoService, public router: Router,
     private slimLoadingBarService: SlimLoadingBarService,){
-    //this.seedDatabase()
-
+    
     this.router.events.subscribe(evt=>{
 
       if(evt instanceof NavigationStart){
@@ -27,19 +26,5 @@ export class AppComponent {
       }
       
     })
-  }
-
-  private seedDatabase() {
-    const dummyPoints = [
-      {name:'Location a', address:'2 Breakthrough Church Avenue Off Cmd Road,Ikosi Ketu', location:[6.6403558, 3.3176431]},
-      {name:'Location b', address:'52, Olasanoye Street', location:[6.651611, 3.325750]},
-      {name:'Location c', address:'49, Baale Street', location:[6.658900,3.3176431]},
-      {name:'Location d', address:'69,Adetokunbo Ademola Crescent', location:[6.658175, 3.292362]},
-      {name:'Location e', address:'Corporation Drive, Dolphin Estate, Ikoyi', location:[6.658175,3.292362]}
-    ]
-  
-    dummyPoints.forEach(({name,location, address}, idx) => {
-      this.geoService.setLocation(name, address, location)
-    })
-  }       
+  }    
 }
